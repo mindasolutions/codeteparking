d6465110c9d229408d7afc0ece9c846bce25e77b
Dawid Minda <dawid@minda.pl>
2018-10-24 02:01:01 +0200

    Added redirect from /codete/ to /codete/home in main directory

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<meta http-equiv="refresh" content="0; url=/codete/home" />
+<title>
+Codete Redirecting</title>
+<style>
+* { font-family: verdana; font-size: 10pt; COLOR: gray; }
+b { font-weight: bold; }
+table { border: 1px solid gray;}
+td { text-align: center; padding: 25;}
+</style>
+</head>
+<body>
+<center>
+<br><br><br><br>
+<table>
+<tr><td>Za chwilę zostaniesz przekierowany.</td></tr>
+</table>
+<br><br>
+</center>
+</body>
+</html>
